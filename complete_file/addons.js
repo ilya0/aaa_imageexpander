e874501610b25564208a68b1d4794321cb6a2dfb
Ilya Osovets <iosovets@gmail.com>
2017-05-18 12:02:31 -0700

    working with sort function

--- complete_file/addons.js
+++ complete_file/addons.js
@@ -1,24 +1,28 @@
 console.log("addons loaded");
-
 var buttonpressed = "none";
-
 $(document).ready(function () {
 
 
     function hideexcept() {
         alert("hiding all except " + buttonpressed);
-        $(buttonpressed).not(".banner").hide();
+        $(".gridder-list").not(buttonpressed).hide();
+        $(buttonpressed).show();
+    }
 
+    function showall() {
+        $(".gridder-list").show();
     }
 
 
 
-
-
     $(".button").click(function () {
-        buttonpressed = $(this).attr('filtertype')
+        if ($(this).attr('filtertype') == "showall") {
+            showall();
+        } else {
+            buttonpressed = $(this).attr('filtertype');
+            hideexcept();
+        }
 
-        hideexcept();
     });
 
 
